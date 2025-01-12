c65e23bb98e6b5fff8c74402317b8b70d03ef811
alex_ <alex_7377@hotmail.com>
2025-01-12 09:28:49 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+import './App.css';
+// import Lesson02 from './lessons/lesson02/Lesson02';
+// import Homework01 from './homeworks/homework01/Homework01';
+import Lesson01 from './lissons/lesson01/Lesson01';
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <Lesson01/>
+      {/* <Homework01/>   */}
+      {/* <Lesson02/> */}
+    </>
+  );
+}
+
+export default App;
