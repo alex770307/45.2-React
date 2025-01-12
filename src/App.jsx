9d23dccfad8266f56b08065e6105aa82620c8428
alex_ <alex_7377@hotmail.com>
2025-01-12 09:47:19 +0100

    hom01

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@
 
 import './App.css';
 // import Lesson02 from './lessons/lesson02/Lesson02';
-// import Homework01 from './homeworks/homework01/Homework01';
-import Lesson01 from './lissons/lesson01/Lesson01';
+// import Lesson01 from './lissons/lesson01/Lesson01';
+import Homework01 from './homeworks/homework01/Homework01';
 
 
 function App() {
@@ -11,8 +11,8 @@ function App() {
 
   return (
     <>
-      <Lesson01/>
-      {/* <Homework01/>   */}
+      {/* <Lesson01/> */}
+      <Homework01/>  
       {/* <Lesson02/> */}
     </>
   );
